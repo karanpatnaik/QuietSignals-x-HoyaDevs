bba48d2e47b50155f37d7e7cdc1a112a8bf536ca
cwlau77 <camwlau7@gmail.com>
2026-04-08 15:48:51 -0400

    Update run_demo.py

--- app/run_demo.py
+++ app/run_demo.py
@@ -1,7 +1,6 @@
 import sys
 from pathlib import Path
 
-# Add parent directory to path so 'model' can be found
 sys.path.insert(0, str(Path(__file__).parent.parent))
 
 from model.train import train_model
